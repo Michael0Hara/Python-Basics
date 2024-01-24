885232169dd595b81a98a0135afd1a5993c2e6b4
Michael0Hara <157568090+Michael0Hara@users.noreply.github.com>
2024-01-24 11:53:23 +0530

    Add files via upload

---
+++ 1.Lists and Variables.py
@@ -0,0 +1,9 @@
+#Program to create a List
+
+# creating any object for example 'fruits'
+Fruits = ["Apple","Cherry","Banana","Mango","Pineapple","Grapes"] 
+# square bracket [] defines that the object is defined as list
+
+for x in Fruits:
+    # the 'x' equates every value one after the other in the list fruits and prints them accordingly 
+    print(x)
