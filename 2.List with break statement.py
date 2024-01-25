630ce687e23bc6c4a3ceb48151fc309c6f336c8c
Aditya Satapathy <157568090+Michael0Hara@users.noreply.github.com>
2024-01-25 16:15:04 +0530

    Add files via upload

---
+++ 2.List with break statement.py
@@ -0,0 +1,8 @@
+#Program to use break statement in list
+
+# creating any object for example 'color'
+color = ["red","yellow","blue","green","pink","orange"]
+for d in color: # expressing the color in the variable 'd'
+    print(d) # printing the values of d=color as stated in list
+    if d == "blue": # if conditional statement: when value of list becomes "blue"
+        break # program will break the computing process and print all the values predecing blue
